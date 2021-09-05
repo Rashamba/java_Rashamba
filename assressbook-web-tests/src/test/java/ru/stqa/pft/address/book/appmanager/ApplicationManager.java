6326190c6b6df0ab186c34cc58ee3177715f8e12
Rashamba <rashamba528@gmail.com>
2021-09-05 22:13:22 +0300

    Домашнее задание №7

--- assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/appmanager/ApplicationManager.java
+++ assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/appmanager/ApplicationManager.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.address.book.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.devtools.v85.browser.Browser;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
@@ -23,11 +22,11 @@ public class ApplicationManager {
   }
 
   public void init() {
-    if (browser == BrowserType.CHROME) {
+    if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
-    } else if (browser == BrowserType.FIREFOX) {
+    } else if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
-    } else if (browser == BrowserType.IE) {
+    } else if (browser.equals(BrowserType.IE)) {
       wd = new InternetExplorerDriver();
     }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
