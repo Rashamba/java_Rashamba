57453792cb4025ac8d2d4544655550d6cd2d8d34
Rashamba <rashamba528@gmail.com>
2021-09-28 14:32:31 +0300

    Оптимизация переходов между страницами

--- assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/tests/ContactCreationTests.java
+++ assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() {
     app.getContactHelper().gotoContactCreation();
-    app.getContactHelper().fillContactForm(new ContactData("igor", "Krasnoborodko", "Rashamba", "Voronezh", "666999", "Egar", "test@gmail.com", "27", "September", "1993", "TestAddress", "777", "test1"), true);
+    app.getContactHelper().fillContactForm(new ContactData("igor", "Krasnoborodko", "Rashamba", "Voronezh", "666999", null, "test@gmail.com", "27", "September", "1993", "TestAddress", "777", "test1"), true);
     app.getContactHelper().submitContactCreation();
     app.getContactHelper().returnToContactPage();
   }
