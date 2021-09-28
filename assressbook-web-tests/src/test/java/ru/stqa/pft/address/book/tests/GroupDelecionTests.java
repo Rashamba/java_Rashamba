f733892904ae248baa35bec2c1b55c429a21366d
Rashamba <rashamba528@gmail.com>
2021-09-28 15:46:10 +0300

    Реализована проверка и обеспечение проверки предусловий

--- assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/tests/GroupDelecionTests.java
+++ assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/tests/GroupDelecionTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.address.book.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.address.book.model.GroupData;
 
 public class GroupDelecionTests extends TestBase{
 
   @Test
   public void testGroupDelecion() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroup();
     app.getGroupHelper().returnToGroupPage();
--- assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/tests/GroupModificatiomTests.java
+++ assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/tests/GroupModificatiomTests.java
@@ -7,6 +7,9 @@ public class GroupModificatiomTests extends TestBase {
   @Test
   public void testGroupModificatoin() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModifacation();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
