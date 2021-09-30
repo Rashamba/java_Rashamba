f7c45fae56a6628126621553166541329ace6bdd
Rashamba <rashamba528@gmail.com>
2021-09-30 18:23:46 +0300

    Лекция 4.4

--- assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/tests/GroupModificatiomTests.java
+++ assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/tests/GroupModificatiomTests.java
@@ -12,7 +12,7 @@ public class GroupModificatiomTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before -1);
     app.getGroupHelper().initGroupModifacation();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModification();
