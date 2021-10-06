1a22eddd698b9d9da3d657d04f5ff016f1dc49ce
Rashamba <rashamba528@gmail.com>
2021-10-06 22:06:03 +0300

    Лекция 5.5. Группы

--- assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/model/GroupData.java
+++ assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/model/GroupData.java
@@ -14,6 +14,26 @@ public class GroupData {
     return name;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    if (name != null ? !name.equals(groupData.name) : groupData.name != null) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
+
   public String getHeader() {
     return header;
   }
@@ -28,21 +48,6 @@ public class GroupData {
             "id='" + id + '\'' +
             ", name='" + name + '\'' +
             '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-  }
-
-  @Override
-  public int hashCode() {
-    return name != null ? name.hashCode() : 0;
   }
 
   public GroupData withId(int id) {
