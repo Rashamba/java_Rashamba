274f418f5dd901d1afb044de64b7bf843a0b0a45
Rashamba <rashamba528@gmail.com>
2021-10-12 22:05:55 +0300

    Лекция 6.7 Группы

--- assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/generators/GroupDataGenerator.java
+++ assressbook-web-tests/src/test/java/ru/stqa/pft/address/book/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.address.book.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.address.book.model.GroupData;
 
@@ -42,9 +44,20 @@ public class GroupDataGenerator {
       saveAsCsv(groups, new File(file));
     } else if ((format.equals("xml"))) {
       saveAsXml(groups, new File(file));
+    } else if ((format.equals("json"))) {
+      saveAsJson(groups, new File(file));
     } else {
+      
       System.out.println("Unrecognize format " + format);
     }
+  }
+
+  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
   }
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {
@@ -68,8 +81,8 @@ public class GroupDataGenerator {
   private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
-      groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header\n %s", i))
-              .withFooter(String.format("footer\n %s", i)));
+      groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header %s", i))
+              .withFooter(String.format("footer %s", i)));
     }
     return groups;
   }
