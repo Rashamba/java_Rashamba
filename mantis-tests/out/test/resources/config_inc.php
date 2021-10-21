96a6acfe2be759776a5b36259eef4e4c509a01c3
Rashamba <rashamba528@gmail.com>
2021-10-21 23:59:59 +0300

    Лекция 8.6 процесс

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -5,5 +5,6 @@ $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
 $g_signup_use_captcha = OFF;
-
 $g_crypto_master_salt = 'secret_value_test';
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -19,6 +19,7 @@ public class ApplicationManager {
   private String browser;
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
+  private MailHelper mailHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -58,6 +59,7 @@ public class ApplicationManager {
     return ftp;
   }
 
+
   public WebDriver getDriver() {
     if (wd == null) {
       if (browser.equals(BrowserType.CHROME)) {
@@ -71,5 +73,12 @@ public class ApplicationManager {
       wd.get(properties.getProperty("web.baseUrl"));
     }
     return wd;
+  }
+
+  public MailHelper mail() {
+    if (mailHelper == null) {
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
   }
 }
